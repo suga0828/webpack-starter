c8015f2734e25705595d0ea2354310d9075610df
Alexander Sandoval <alexander.sf0828@gmail.com>
2020-06-01 21:12:10 -0500

    Add webpack-dev-server and optimize for development mode

--- webpack.config.js
+++ webpack.config.js
@@ -1,21 +1,38 @@
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 
 module.exports = {
   mode: 'development',
   target: 'web', // <=== can be omitted as default is 'web'
   entry: {
-    main: './src/index.js',
+    app: './src/index.js',
     vendor: './src/vendor.js',
+  },
+  devtool: 'inline-source-map',
+  devServer: {
+    contentBase: path.join(__dirname, 'dist'),
+    compress: true,
+    port: 9999,
   },
   output: {
     path: __dirname + '/dist',
-    filename: '[name].js',
+    filename: '[name].bundle.js',
   },
+  plugins: [
+    new CleanWebpackPlugin({ cleanStaleWebpackAssets: false }),
+    new HtmlWebpackPlugin({ title: 'Output Management' }),
+  ],
   module: {
     rules: [
       { test: /\.s[ac]ss$/i, use: ['style-loader', 'css-loader', 'sass-loader'] },
       { test: /\.(png|svg|jpg|gif)$/, use: ['file-loader'] },
       { test: /\.(woff|woff2|eot|ttf|otf)$/, use: ['file-loader'] },
     ],
+  },
+  optimization: {
+    splitChunks: {
+      chunks: 'all',
+    },
   },
 };
