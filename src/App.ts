9e8beeff14c26ba2777c2a90c99cb17e04cde511
Alexander Sandoval <alexander.sf0828@gmail.com>
2020-07-31 19:36:37 -0500

    Rename api file and fix app template

--- src/App.ts
+++ src/App.ts
@@ -1,22 +1,8 @@
+import { ListUsers } from "./components/ListUsers";
+
 export const App = (): Element => {
-  const container = document.createElement('div');
-
-  const template = `
-    <section>
-      <h1>Tutorial: testing de código asíncrono</h1>
-      <p>En este tutorial vamos a hacer test unitarios de peticiones HTTP hechas con <a href="">Fetch API</a>. Vamos a utilizar una API REST pública: <a href="https://jsonplaceholder.typicode.com/guide.html">JSONPlaceholder - Users</a>.</p>
-      <p>En la medida de lo posible voy a tratar de escribir y generar el código de este tutorial en español.</p>
-    </section>
-    <section>
-      <h2>Obtener Usuarios</h2>
-      <h3>Lista de Usuarios</h3>
-      <ul><>
-    </section>
-    <section></section>
-    <section></section>
-  `;
-
-  container.innerHTML = template;
+  const container = document.createElement('main');
+  container.insertAdjacentElement('beforeend', ListUsers());
 
   return container;
 };
---
+++ src/api/api.service.ts
@@ -0,0 +1,30 @@
+import { User } from "./api.model";
+
+const BASE_API = 'https://jsonplaceholder.typicode.com/users/';
+
+export const irABuscarUsuarios = (): Promise<any> => fetch(BASE_API).then(resp => resp.json())
+
+export const irABuscarUsuario = (id: string): Promise<any> => fetch(BASE_API + id).then(resp => resp.json())
+
+export const crearUsuario = async (user: User): Promise<any> => {
+  const config = {
+    method: 'POST',
+    headers: { 'Content-Type': 'application/json' },
+    body: JSON.stringify(user)
+  }
+  const resp = await fetch(BASE_API, config);
+  return await resp.json();
+}
+
+export const actualizarUsuario = async (user: Partial<User>): Promise<any> => {
+  const config = {
+    method: 'PATCH',
+    headers: { 'Content-Type': 'application/json' },
+    body: JSON.stringify({ ...user })
+  }
+  const response = await fetch(BASE_API + user.id, config);
+  return await response.json();
+}
+
+export const eliminarUsuario = (id: number): Promise<any> => fetch(BASE_API + id, { method: 'DELETE' })
+  .then(response => response.json())
