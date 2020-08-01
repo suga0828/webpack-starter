6950d760a252c1c17279430922f0a6800acfb5d5
Alexander Sandoval <alexander.sf0828@gmail.com>
2020-08-01 11:39:42 -0500

    create ListUsers component tests

---
+++ src/components/__tests__/ListUsers.test.ts
@@ -0,0 +1,47 @@
+import * as Component from '../ListUsers';
+
+import * as API from '../../api/api.service';
+import { User } from '../../api/api.model';
+
+describe('ListUsers component', () => {
+  describe('imprimirUsiariosEnContenedor method test suite', () => {
+    it('should print users response inside container', async () => {
+      jest.spyOn(API, 'irABuscarUsuarios')
+        .mockImplementation((): Promise<User[]> => (
+          new Promise(resolve => resolve([{ name: 'mock user' } as User]))
+      ));
+
+      const container: HTMLUListElement = document.createElement('ul');
+
+      Component.imprimirUsiariosEnContenedor(container).then((e) => {
+        expect(container.innerHTML).toBe(`<li>mock user</li>`);
+      });
+    });
+  });
+
+  describe('ListUsers method test suite', () => {
+    it('should return a template with a button with id botonObtener', () => {
+      const template = Component.Render();
+
+      expect(template.querySelector('#botonObtener')).not.toBeNull();
+    });
+
+    it('should return a template with an ul with id listaUsuarios', () => {
+      const template = Component.Render();
+
+      expect(template.querySelector('#listaUsuarios')).not.toBeNull();
+    });
+
+    describe('when make click on a button with id botonObtener', () => {
+      it('should call imprimirUsiariosEnContenedor', () => {
+        const spy = jest.spyOn(Component, 'imprimirUsiariosEnContenedor');
+        const boton = Component.Render().querySelector('#botonObtener');
+
+        boton.dispatchEvent(new Event('click'));
+  
+        expect(spy).toHaveBeenCalled();
+      });
+    })
+  });
+});
+
